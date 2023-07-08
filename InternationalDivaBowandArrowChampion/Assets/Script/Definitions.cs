e60c230d33c80efdbb9a534e70bcca37732a9a38
Jingbo(Bobo) Liu <719744328ccs@gmail.com>
2023-07-08 13:01:02 +0800

    Definitions

---
+++ InternationalDivaBowandArrowChampion/Assets/Script/Definitions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 疾病/症状
+public enum Symptom
+{
+    Cold = 0,
+    Fever = 10,
+    Exhausted = 20,
+    Insomnia = 30 // 和一名老师学的
+}
+
+
+// 药草
+public enum Herb
+{
+    Ginseng = 0, // 人参
+    Angelica = 10, // 当归
+    Gentian = 20, // 龙胆
+    Coptis = 30, // 黄连
+}
---
+++ InternationalDivaBowandArrowChampion/Assets/Script/Pharmacy/Pharmacy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pharmacy : MonoBehaviour
+{
+    public void Init()
+    {
+        // Clear plate
+        
+        // Reset all drawer
+        
+        // 
+    }
+}
